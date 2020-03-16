5d4d9f8a67642912df671fa976cb2118ff52ea33
karthik-karotra <karthikpatel54@gmail.com>
2020-03-16 13:28:59 +0530

    Code & Testcases for equality check on temperature in different units

--- src/main/java/com/quantitymeasurement/Units.java
+++ src/main/java/com/quantitymeasurement/Units.java
@@ -1,9 +1,14 @@
 package com.quantitymeasurement;
 
 public enum Units {
+    //Enums for conversions according to lengths
     INCH(1.0), FEET_TO_INCH(1 * 12.0), YARD_TO_INCH(1 * 36.0),CM_TO_INCH(1/2.5),
+    //Enums for conversions according to volumes
     LITRE(1.0), GALLON_TO_LITRE(1 * 3.78), MILLILITER_TO_LITRE(1.0/1000),
-    KILOGRAM(1.0),GRAMS_TO_KILOGRAM(1.0/1000),TONNE_TO_KILOGRAM(1.0*1000);
+    //Enums for conversions according to weights
+    KILOGRAM(1.0),GRAMS_TO_KILOGRAM(1.0/1000),TONNE_TO_KILOGRAM(1.0*1000),
+    //Enums for conversions according to temperature
+    CELSIUS(1.0*2.12), FAHRENHEIT_TO_CELSIUS(1.0);
 
     final Double unit;
 
